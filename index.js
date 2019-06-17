6e401e52f8cd0be0a5822f9a0b0121d364b74762
Chamath Jeevan <chamathjeevan@gmail.com>
2019-06-17 10:48:02 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//const clientRoutes = require('./routers/client.js')
+//app.use(clientRoutes);
 
 
-Suppliers:  ID, Client_ID, Company_NAME, CompanyInFold, NearestSeaPort, NearestAirPort, SeaTransitTime, AirTransitTime, SuppliersWebSite, LinkToCatalogues, Parent_ID, IsDecaseed, IsActive
-Entities:  Name, Client_ID, Address, TIN, BusinessRegNo, Status
-PaymentTerms: ID, Client_ID, Supplier_ID, PaymentTerm, DueDays, DueStartOn, PaymentType, Status
-ContactDetails: ID, Company_NAME, ContactNo, Address, Email
 
-Companies: Client_ID, NAME, Address
-Clients: ID, ClientInfoID, Address, VAT, TIN, BusinessRegNo, Parent_ID, IsDeleted, IsActive, CreatedBy, CreatedTime
+var intPostStartUp = function intPostStartUp() {
+    // bind error middleware
+    app.use(_ErrorMiddleware.ErrorMiddleware);
+  };
+  
+  exports.intPostStartUp = intPostStartUp;
 
-AffiliatedCompany: Client_ID, Incoterm_ID, PaymentTerm_ID, Company_NAME, Type, Bank, IsDeleted, IsActive
-
-Banks: NAME, BusinessRegNo, TIN, SWIFT
-
-Forwarders: ID, Client_ID, Company_NAME, TypeOfVendor, BusinessRegNo, TIN
-
-Incoterms: ID, Incoterms, Description, Status
-
-
-I have edited the ER diagram now. See if it makes sense @Chamath Jeevan. Affiliated company table is there  to represent connection with each companies. Not all clients can make orders from all the suppliers in the db. Only a set of suppliers can do business with certain clients. These relationships are defined with payment terms and inco terms. So we need to have this relationship information on some place. That is served by affiliated company table. @Shavin, @Chamath Correct me if I am wrng in business case I mentioned above
+// set port
+app.listen(3000, function() {
+    console.log('Node app is running on port 3000');
+});
+module.exports = app;
