dca7b5f44e8d3d1bb46383184820f1d540bf3a9e
nishakara <nishakara.ac@gmail.com>
2019-06-25 15:05:56 +0530

    adding unit of measures

--- routers/material.js
+++ routers/material.js
@@ -32,7 +32,8 @@ router.post('/material/:Id', function(req, res, next) {
 
 router.put('/material/:Id', function(req, res) {
   let material = req.body;
-  dbConnection.query('UPDATE Material SET ID = ?, ItemName = ?, ItemType = ? ,ItemOrigin = ?, UnitOfMeasure_ID = ?, HS_HsCode = ? ,RequiredApprovalTypes = ?, CargoType= ?, Trade_Agreement_Type_ID = ?, IsDeleted = ?, IsActive = ?, IsBOI = ?, Parent_ID = ?, Client_ID = ?, CreatedBy = ?, CreatedTime = ?', [Material.ID, Material.ItemName, Material.ItemType, Material.ItemOrigin, Material.UnitOfMeasure_ID, Material.HS_HsCode, Material.RequiredApprovalTypes, Material.CargoType, Material.Trade_Agreement_Type_ID, Material.IsDeleted, Material.IsActive, Material.IsBOI, Material.Parent_ID, Material.Client_ID, Material.CreatedBy, Material.CreatedTime], function(error, results, fields) {
+  dbConnection.query('UPDATE Material SET ID = ?, ItemName = ?, ItemType = ? ,ItemOrigin = ?, UnitOfMeasure_ID = ?, HS_HsCode = ? ,RequiredApprovalTypes = ?, CargoType= ?, Trade_Agreement_Type_ID = ?, IsDeleted = ?, IsActive = ?, IsBOI = ?, Parent_ID = ?, Client_ID = ?, CreatedBy = ?, CreatedTime = ?', 
+  [Material.ID, Material.ItemName, Material.ItemType, Material.ItemOrigin, Material.UnitOfMeasure_ID, Material.HS_HsCode, Material.RequiredApprovalTypes, Material.CargoType, Material.Trade_Agreement_Type_ID, Material.IsDeleted, Material.IsActive, Material.IsBOI, Material.Parent_ID, Material.Client_ID, Material.CreatedBy, Material.CreatedTime], function(error, results, fields) {
     if (error) return next(error);
     if (!results || results.affectedRows == 0) res.status(400).send();
     return res.send(results);
